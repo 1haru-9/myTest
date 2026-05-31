0d3166cc2df4edc493065ac02320dcacd6a33851
1haru-9 <seinenharuta@gmail.com>
2026-05-31 19:29:05 +0900

    保存

--- Assets/Scripts/07_Gravity/PingPongRidableFloor2.cs
+++ Assets/Scripts/07_Gravity/PingPongRidableFloor2.cs
@@ -4,15 +4,58 @@ using UnityEngine;
 
 public class PingPongRidableFloor2 : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // ------------------------------------
+    public float speed = 1.0f; // [速度]
+    public float moveTime = 2.0f; // [移動時間]
+    // ------------------------------------
+    Rigidbody2D rbody;
+    private float timer =0.0f;
+    private List<GameObject> childObjects = new List<GameObject>();
+
     void Start()
     {
-        
+        // 重力や外力の影響を受けずにスクリプトで移動
+        rbody = GetComponent<Rigidbody2D>();
+        rbody.bodyType = RigidbodyType2D.Kinematic;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        if (timer >= moveTime)
+        {
+            speed = -speed; // 方向転換
+            timer = 0.0f;
+        }
+        CheckRemove(); // 接触しなくなったら一緒に移動を解除
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+
+    // 接触したら、一緒に移動
+    public void OionEnter2D(Collision2D other)
+    {
+        childObjects.Add(other.gameObject);
+        other.gameObject.transform.parent = transform;
+    }
+
+    // 接触しなくなったら、一緒に移動を解除
+    private void CheckRemove()
+    {
+        List<GameObject> removeList = new List<GameObject>();
+        foreach (GameObject obj in childObjects)
+        {
+            Collider2D childCol = obj.GetComponent<Collider2D>();
+            Collider2D floorCol = GetComponent<Collider2D>();
+            if (!Physics2D.IsTouching(childCol, floorCol))
+            {
+                obj.transform.parent = null;
+                removeList.Add(obj);
+            }
+        }
+        foreach (GameObject obj in removeList)
+        {
+            childObjects.Remove(obj);
+        }
     }
 }
