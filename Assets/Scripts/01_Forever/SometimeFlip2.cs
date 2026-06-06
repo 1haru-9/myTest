df10f232a6b878eefb638d04292a92dc3143f6b9
1haru-9 <seinenharuta@gmail.com>
2026-06-06 14:50:13 +0900

    プッシュ

---
+++ Assets/Scripts/01_Forever/SometimeFlip2.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SometimeFlip2 : MonoBehaviour
+{
+    //-------------------------------------
+    public int maxCount = 50; // [頻度
+    //-------------------------------------
+    int count = 0; // カウンター用
+    bool flipFlag = false;
+
+    void Start()
+    {
+        count = 0;  // カウンターをリセット
+    }
+
+    void FixedUpdate()
+    {
+		count = count + 1; // カウンターに1を足して
+		if (count >= maxCount)  // もし、maxCountになったら
+		{
+			transform.Rotate(0, 0, 180); // 180度回転して曲がる
+			count = 0; // カウンターをリセット
+			flipFlag = !flipFlag;	// 上下を反転
+			GetComponent<SpriteRenderer>().flipY = flipFlag; 
+		}
+	}
+}
---
+++ Assets/Scripts/07_Gravity/CameraManager2.cs
@@ -0,0 +1,42 @@
+using UnityEditor.Analytics;
+using UnityEngine;
+
+// カメラが動くものを追いかける
+public class CameraManager2 : MonoBehaviour
+{
+    //--------------------------------------
+    public GameObject player; // [プレイヤー]
+    public Vector2 offset; // [オフセット]
+    public float xlimit = 0; // [x軸制限]
+    public bool followHOnly = true; // [水平追跡のみ]
+    public bool smoothFollow = false; // [滑るかフラグ]
+    public float smoothSpeed = 4f; // [滑らか度]
+    //--------------------------------------
+    void LateUpdate()
+    {
+        if (player == null) return; // プレイヤーがいないなら終了
+        Vector3 playerPos = player.transform.position;
+        Vector3 desiredPos;
+        float vx = playerPos.x + offset.x;
+        float vz = transform.position.z;
+        if (vx < xlimit) vx = xlimit;
+        if (followHOnly)
+        {
+            desiredPos = new Vector3(vx, transform.position.y, vz);
+        }
+        else
+        {
+            desiredPos = new Vector3(vx, playerPos.y + offset.y, vz);
+        }
+        if (smoothFollow)
+        {
+            Vector3 smoothedPos = Vector3.Lerp(transform.position,
+                        desiredPos, smoothSpeed * Time.deltaTime);
+            transform.position = smoothedPos;
+        }
+        else
+        {
+            transform.position = desiredPos;
+        }
+    }
+}
